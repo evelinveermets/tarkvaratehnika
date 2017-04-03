ed2427dc997a923d5713691183a7ac9df420c6b9
lytempel <lytempel@gmail.com>
2017-04-03 18:04:22 +0300

    Remove autogenerated scripts

--- 3Fit/src/app.js
+++ 3Fit/src/app.js
@@ -1,5 +1,16 @@
 export class App {
-  constructor() {
-    this.message = 'Tere tulemast!';
+    constructor() {
+        this.message = 'Tere tulemast!';
   }
+    
+    
+    configureRouter(config, router) {
+        this.router = router;
+        config.title = '3Fit Aurelia';
+        config.map([
+          { route: ['', 'home'],       name: 'home',       moduleId: 'home/index' },
+          { route: 'users',            name: 'users',      moduleId: 'users/index',   nav: true },
+        ]);
+  }
+    
 }
