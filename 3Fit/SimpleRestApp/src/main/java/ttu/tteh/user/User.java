3ba1be6f178641451ebd589a3b08a2ffa665714b
kristelmerilain <merilainkristel@gmail.com>
2017-04-09 19:45:23 +0300

    Add files via upload

---
+++ 3Fit/SimpleRestApp/src/main/java/ttu/tteh/user/User.java
@@ -0,0 +1,38 @@
+package ttu.tteh.user;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+import lombok.Getter;
+import lombok.Setter;
+import ttu.tteh.car.Car;
+import ttu.tteh.pen.Pen;
+
+@Entity
+@Getter
+@Setter
+public class User {
+	@Id
+	@GeneratedValue
+	long id;
+	String email;
+	String password;
+	String passwordConf;
+    String firstname;
+    String lastname;
+	
+	@OneToOne(mappedBy="user",  // välja nimi Car klassis
+			cascade=CascadeType.ALL)
+	Car car;
+	
+	@OneToMany(mappedBy="user", cascade=CascadeType.ALL)
+	List<Pen> pens;
+
+	
+}
