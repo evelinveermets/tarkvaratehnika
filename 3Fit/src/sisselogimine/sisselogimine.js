a6121cb478b8c94a56151c751b5df7e60316be57
kristelmerilain <merilainkristel@gmail.com>
2017-04-09 19:44:20 +0300

    Update sisselogimine.js

--- 3Fit/src/sisselogimine/sisselogimine.js
+++ 3Fit/src/sisselogimine/sisselogimine.js
@@ -1,6 +1,18 @@
+import {HttpClient, json} from 'aurelia-fetch-client'
+
 export class Sisselogimine {
-    constructor() {
-        this.message = "Testing..."
-    }
+    userData = {}
+
+    addUser(){
+        let client = new HttpClient();
+        client.fetch('http://localhost:8080/users/add',{
+            'method': "POST",
+            'body': json(this.userData)
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log("Server saatis"+data.email)
+        });
+        console.log("Method executed!")
     
 }
