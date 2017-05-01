ebf742b23ea782bfd9de3d4d1b38d69cc212702f
kristelmerilain <merilainkristel@gmail.com>
2017-05-01 23:03:44 +0300

    logout lisamine

--- 3Fit/src/treener_minukonto/treener_minukonto.js
+++ 3Fit/src/treener_minukonto/treener_minukonto.js
@@ -1,5 +1,11 @@
+import {inject} from 'aurelia-framework';
+import {LoginService2} from '../myTrainerServices';
 
 export class Treener_minukonto {
-    
+    logout(){
+    LoginService.logout();
+    alert("Logged out");
+    this.router.navigateToRoute('#/treener_sisselogimine');
+  }
     
 }
