6c153b370b22b4d48eba75f80553045492d9751e
kristelmerilain <merilainkristel@gmail.com>
2017-05-01 23:04:57 +0300

    logout lisamine

--- 3Fit/src/treener_tellijad/treener_tellijad.js
+++ 3Fit/src/treener_tellijad/treener_tellijad.js
@@ -1,4 +1,11 @@
+import {inject} from 'aurelia-framework';
+import {LoginService2} from '../myTrainerServices';
+
 export class Treener_tellijad {
-    
+    logout(){
+    LoginService.logout();
+    alert("Logged out");
+    this.router.navigateToRoute('#/treener_sisselogimine');
+  }
     
 }
