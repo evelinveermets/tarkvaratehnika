17ae74c9e31fb00f312be0c7bb260f385e5dc8f4
kristelmerilain <merilainkristel@gmail.com>
2017-05-01 23:02:33 +0300

    logout lisamine

--- 3Fit/src/treener_avaleht/treener_avaleht.js
+++ 3Fit/src/treener_avaleht/treener_avaleht.js
@@ -1,5 +1,10 @@
-
+import {inject} from 'aurelia-framework';
+import {LoginService2} from '../myTrainerServices';
 export class Treener_avaleht {
-    
+    logout(){
+        LoginService.logout();
+        alert("Logged out");
+        this.router.navigateToRoute('#/treener_sisselogimine');
+  }
     
 }
