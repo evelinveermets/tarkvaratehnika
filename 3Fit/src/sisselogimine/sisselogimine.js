9e5ce43196387ff917b80de33db0e1c8b40d91eb
evelinveermets <evelin.veermets@ttu.ee>
2017-05-11 11:02:07 +0300

    Update sisselogimine.js
    Parooli kontroll

--- 3Fit/src/sisselogimine/sisselogimine.js
+++ 3Fit/src/sisselogimine/sisselogimine.js
@@ -29,4 +29,54 @@ export class Sisselogimine {
         .then(success => window.location.reload(true))
         .catch(error => console.warn(error.message));
   }
+function isPasswordValid(input) {
+  if (!hasUpperCase(input)) {
+    console.log('Kasuta suurt tähte.');
+  }
+  if (!hasLowerCase(input)) {
+    console.log('Kasuta väikest tähte.');
+  }
+  if (!isLongEnough(input)) {
+    console.log('Parool ei ole piisavalt pikk.');
+  }
+  if (!hasSpecialCharacter(input)) {
+    console.log('Kasuta sümbolit.');
+  }   
+  if (hasUpperCase(input) && hasLowerCase(input) && isLongEnough(input) && hasSpecialCharacter(input)) {
+    console.log('Parool on sobiv.');
+  } 
+}
+
+function hasUpperCase(input) {
+  for (var i = 0; i < input.length; i++) {
+    if (input[i] === input[i].toUpperCase()) {
+      return true;
+    }
+  }
+}
+
+function hasLowerCase(input) {
+  for (var i=0; i < input.length; i++) {
+    if (input[i] === input[i].toLowerCase()) {
+      return true;
+    }
+  }
+}
+
+function isLongEnough(input) {
+  for (var i=0; i<input.length; i++) {
+    if (input.length>=8) {
+      return true;
+    }
+  }
+}
+
+function hasSpecialCharacter(input) {
+  var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+'];
+  for (var i=0; i<input.length; i++) {
+    for (var j=0; j<specialCharacters.length; j++) {
+      if (input[i]===specialCharacters[j]) {
+        return true;
+      }
+    }
 }
