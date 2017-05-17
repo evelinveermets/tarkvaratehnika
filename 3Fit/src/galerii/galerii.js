f1b88132deac4f3d02e57243ec28c76516185657
kristelmerilain <merilainkristel@gmail.com>
2017-05-17 09:07:05 +0300

    Update galerii.js

--- 3Fit/src/galerii/galerii.js
+++ 3Fit/src/galerii/galerii.js
@@ -1,7 +1,25 @@
 import {inject} from 'aurelia-framework';
-import {LoginService} from '../myServices';
+import {LoginService} from '../LoginService';
+import {Router} from 'aurelia-router';
 
-export class galerii{
+@inject(Router)
+export class Avaleht {
+    router;
+
+
+    constructor(router : Router) {
+        this.router = router;
+        this.clientName = localStorage.getItem("userFirstName");
+    }
+    activate(){
+
+    }
+
+  logout(){
+    LoginService.logout();
+    alert("Olete edukalt välja logitud");
+    this.router.navigateToRoute('home');
+  }
 
 
 }
